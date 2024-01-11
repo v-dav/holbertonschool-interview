85fbd1a231f54bea131c3edca6fec806fbb28a53
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-01-11 17:19:02 +0100

    reduce number of lines

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -6,9 +6,6 @@
  * @number: integer to be included in the new node
  *
  * Return: address of the new list or NULL if it fails
- *
- * Time complexity: O(n)
- * Space complexity: O(1)
  */
 listint_t *insert_node(listint_t **head, int number)
 {
@@ -17,22 +14,17 @@ listint_t *insert_node(listint_t **head, int number)
 
 	if (new == NULL)
 		return (NULL);
-
 	new->n = number;
 	new->next = NULL;
-
 	if (*head == NULL || (current->n > new->n))
 	{
 		new->next = current;
 		*head = new;
-		current = NULL;
+		return (new);
 	}
-	else
-	{
-		while (current->next != NULL && current->next->n < new->n)
-			current = current->next;
-		new->next = current->next;
-		current->next = new;
-	}
-	return (*head);
+	while (current->next != NULL && current->next->n < new->n)
+		current = current->next;
+	new->next = current->next;
+	current->next = new;
+	return (new);
 }
