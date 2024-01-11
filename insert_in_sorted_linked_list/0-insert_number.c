0a0e3c8f90d1ff0d3fa9fa2afa70b7af85981300
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-01-11 17:13:33 +0100

    factorize code under 40 lines

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -4,27 +4,24 @@
  * insert_node - inserts a node in a sorted linked list at the right position
  * @head: pointer to pointer of the first node of listint_t list
  * @number: integer to be included in the new node
- * Return: address of the new element or NULL if it fails
+ *
+ * Return: address of the new list or NULL if it fails
  *
  * Time complexity: O(n)
  * Space complexity: O(1)
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new = NULL;
-	listint_t *current = NULL;
+	listint_t *current = *head;
+	listint_t *new = malloc(sizeof(listint_t));
 
-	current = *head;
-	new = (listint_t *)malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->n = number;
 	new->next = NULL;
 
-	if (*head == NULL)
-		*head = new;
-	else if (current->n > new->n)
+	if (*head == NULL || (current->n > new->n))
 	{
 		new->next = current;
 		*head = new;
@@ -37,6 +34,5 @@ listint_t *insert_node(listint_t **head, int number)
 		new->next = current->next;
 		current->next = new;
 	}
-
 	return (*head);
 }
