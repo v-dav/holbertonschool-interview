97c10e7d682bed66b49e685bd521422d0ed4fce9
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-01-11 16:24:33 +0100

    write the algorithm for insertion of a new node in a sorted linked list in a good position

---
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * insert_node - inserts a node in a sorted linked list at the right position
+ * @head: pointer to pointer of the first node of listint_t list
+ * @number: integer to be included in the new node
+ * Return: address of the new element or NULL if it fails
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new = NULL;
+	listint_t *current = NULL;
+
+	current = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = number;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		while (current->next != NULL && current->next->n < new->n)
+			current = current->next;
+		new->next = current->next;
+		current->next = new;
+	}
+
+	return (new);
+}
