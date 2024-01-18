511da978fdff6b6d5a97bdba3f1afb24092353dd
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-01-18 20:02:05 +0100

    first draft of the heapify up function

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,6 +1,16 @@
 #include "binary_trees.h"
 
-//function to insert the new node into the heap to the first available space
+//==========================================================================
+
+/**
+ * node_insert - a function that inserts a node to the binary tree
+ * to satisfy complete binary tree condition
+ *
+ * @root: a double pointer to the root node of the tree
+ * @node_to_insert: is the node to insert 
+ *
+ * Return: void
+ */
 void node_insert(heap_t **root, heap_t *node_to_insert)
 {
 	if (*root == NULL)
@@ -19,6 +29,44 @@ void node_insert(heap_t **root, heap_t *node_to_insert)
 	else
 		node_insert(&(*root)->left, node_to_insert);
 }
+
+//==========================================================================
+
+void heapify_up(heap_t *node, heap_t **root)
+{
+    while (node->parent && node->n > node->parent->n)
+    {
+	//if node attached to left branch of its parent on first iteration
+	if (node->parent->left == node)
+	{
+		//on the first iteration
+		if (node->left == NULL)
+		{
+			node->parent->left = node->left;
+			node->left = node->parent;
+			node->parent = node->left->parent;
+			node->left->parent = node;
+		}
+		// on the next iterations
+		else
+		{
+
+		}
+	}
+	//if the node attached to right branch on first iteration
+	else
+	{
+
+	}
+    }
+
+    //the node becomes root
+    if (!node->parent)
+        *root = node;
+}
+
+//==========================================================================
+
 
 /**
  * heap_insert - a function that inserts a value into a Max Binary Heap
@@ -46,7 +94,8 @@ heap_t *heap_insert(heap_t **root, int value)
 	//If the root exists, insert the new node to the first available space, beggining from left
 	node_insert(&(*root), new_node);
 
+	//Place the node in the right position to satisfy max_heap condition
+	heapify_up(new_node, root);
+
 	return (new_node);
 }
-
-
