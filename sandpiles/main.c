d42e6647ea313a20cc9a4248a9daa4f07053ab61
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-01 11:57:43 +0100

    write main with examples

--- sandpiles/main.c
+++ sandpiles/main.c
@@ -70,12 +70,30 @@ int main(void)
         {1, 3, 1}
     };
 
-    print_grid_sum(grid1, grid2);
+     int grid3[3][3] = {
+        {4, 4, 4},
+        {4, 4, 4},
+        {4, 4, 4}
+    };
+    int grid4[3][3] = {
+        {4, 4, 4},
+        {4, 4, 4},
+        {4, 4, 4}
+    };
 
+    // ex grid1 et grid2
+    print_grid_sum(grid1, grid2);
     sandpiles_sum(grid1, grid2);
 
     printf("=\n");
     print_grid(grid1);
+
+    // ex grid3 et grid4
+    print_grid_sum(grid3, grid4);
+    sandpiles_sum(grid3, grid4);
+
+    printf("=\n");
+    print_grid(grid3);
 
     return (EXIT_SUCCESS);
 }
