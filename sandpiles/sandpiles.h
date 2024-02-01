591869684030759586f78b334b15568bef9d4baf
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-01 11:57:10 +0100

    fix header file for compilation

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -4,8 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
-static void print_grid(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void add_matrix(int grid1[3][3], int grid2[3][3]);
 
 #endif
