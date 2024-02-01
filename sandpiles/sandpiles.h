89279d83e078a5debcd9655f9871f0ed34be8de8
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-01 11:07:05 +0100

    add prototypes print functions

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
+static void print_grid(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 
 #endif
