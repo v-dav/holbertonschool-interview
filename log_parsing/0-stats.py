c9e9581ec1eb4a3c2e6470ed35d206c4532cd74f
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-15 12:43:33 +0100

    try to fix the code

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -5,42 +5,46 @@
 import signal
 import sys
 
-
-loop_counter = 0
-stats = {}
-file_size_counter = 0
-
-
-def printer():
+def printer(counter, dict_data):
     """ Affiche les informations nécessaires """
 
-    print("File size: {}".format(file_size_counter))
-    for k, v in dict(sorted(stats.items())).items():
+    print("File size: {}".format(counter))
+    for k, v in dict(sorted(dict_data.items())).items():
         print("{}: {}".format(k, v))
 
 
 def ctrlc_handler(signal, frame):
     """ Handler function that detects the signal"""
-    printer()
+    printer(file_size_counter, stats)
 
 
 # Ctrl-C signal listener
 signal.signal(signal.SIGINT, ctrlc_handler)
 
 # The main loop
-for line in sys.stdin:
-    loop_counter += 1
+def main():
+    global loop_counter
+    loop_counter = 0
+    global stats
+    stats = {}
+    global file_size_counter
+    file_size_counter = 0
+    
+    for line in sys.stdin:
+        loop_counter += 1
 
-    line_list = line.split()
-    status_code = int(line_list[7])
-    file_size = int(line_list[8])
+        line_list = line.split()
+        status_code = int(line_list[7])
+        file_size = int(line_list[8])
 
-    if status_code in stats:
-        stats[status_code] += 1
-    else:
-        stats[status_code] = 1
+        if status_code in stats:
+            stats[status_code] += 1
+        else:
+            stats[status_code] = 1
 
-    file_size_counter += file_size
+        file_size_counter += file_size
 
-    if loop_counter % 10 == 0:
-        printer()
+        if loop_counter % 10 == 0:
+            printer(file_size_counter, stats)
+
+main()
