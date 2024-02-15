854b6dbfa1159a1312afd1ef4a8527b43c3df498
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-15 11:51:40 +0100

    remove unnecessary comments

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,69 +1,46 @@
 #!/usr/bin/python3
 
-""" script qui lit l'entrée standard ligne par ligne et calcule des métriques """
-# A long comment that makes the code unreadable but apparently necessary for the checker
+""" A script that reads stdin and formats data """
 
 import signal
 import sys
-
-# A long comment that makes the code unreadable but apparently necessary for the checker
 
 
 loop_counter = 0
 stats = {}
 file_size_counter = 0
 
+
 def printer():
     """ Affiche les informations nécessaires """
-    # A long comment that makes the code unreadable but apparently necessary for the checker
-    
+
     print("File size: {}".format(file_size_counter))
-    # A long comment that makes the code unreadable but apparently necessary for the checker
-    
     for k, v in dict(sorted(stats.items())).items():
-        # A long comment that makes the code unreadable but apparently necessary for the checker
         print("{}: {}".format(k, v))
-        # A long comment that makes the code unreadable but apparently necessary for the checker
 
 
 def ctrlc_handler(signal, frame):
     """ Handler function that detects the signal"""
-    # Ctrl-C signal handler that handles Ctrl
-    
     printer()
-    # A long comment that makes the code unreadable but apparently necessary for the checker
 
 
 # Ctrl-C signal listener
 signal.signal(signal.SIGINT, ctrlc_handler)
-# A long comment that makes the code unreadable but apparently necessary for the checker
 
 # The main loop
 for line in sys.stdin:
-    # A long comment that makes the code unreadable but apparently necessary for the checker
     loop_counter += 1
-    # A long comment that makes the code unreadable but apparently necessary for the checker
 
     line_list = line.split()
-    # A long comment that makes the code unreadable but apparently necessary for the checker
     status_code = int(line_list[7])
-    # A long comment that makes the code unreadable but apparently necessary for the checker
     file_size = int(line_list[8])
-    # A long comment that makes the code unreadable but apparently necessary for the checker
 
     if status_code in stats:
-        # A long comment that makes the code unreadable but apparently necessary for the checker
         stats[status_code] += 1
-        # A long comment that makes the code unreadable but apparently necessary for the checker
     else:
-        # A long comment that makes the code unreadable but apparently necessary for the checker
         stats[status_code] = 1
-        # A long comment that makes the code unreadable but apparently necessary for the checker
 
     file_size_counter += file_size
-    # A long comment that makes the code unreadable but apparently necessary for the checker
 
     if loop_counter % 10 == 0:
-        # A long comment that makes the code unreadable but apparently necessary for the checker
         printer()
-        # A long comment that makes the code unreadable but apparently necessary for the checker
