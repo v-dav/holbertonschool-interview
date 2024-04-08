98b51baba0474a48171191d58f6eefc52701aabc
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-08 13:15:51 +0200

    write constraints verifications

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
 """Nqueens Algorithm"""
 
+import sys
+
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+
+try:
+    N = int(sys.argv[1])
+except ValueError:
+    print("N must be a number")
+    exit(1)
+
+if N < 4:
+    print("N must be at least 4")
+    exit(1)
