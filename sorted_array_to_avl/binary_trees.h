a436ff68b595f8eb90b02f052a4a3018be9e1247
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-09 14:28:53 +0200

    write algo header file

--- sorted_array_to_avl/binary_trees.h
+++ sorted_array_to_avl/binary_trees.h
@@ -15,16 +15,16 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
 
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
 avl_t *sorted_array_to_avl(int *array, size_t size);
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif
