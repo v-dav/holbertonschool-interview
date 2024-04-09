d256cde6db8b1aba6c351d774f03750d52bdf535
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-09 14:50:22 +0200

    factorize code and manage node parent bug

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,31 +1,34 @@
 #include "binary_trees.h"
 
 /**
- * convert_to_bst - helper function to convert to bst
+ * convert_to_avl - helper function to convert to avl
  *
  * @array: Pointer to the first element of the array to be converted
- * @beginning_index: the beginning index of the array
- * @end_index: the end index of the array
+ * @begin_idx: the beginning index of the array
+ * @end_idx: the end index of the array
+ * @parent: pointer to the node's parent
  *
  * Return: the address of the new node or NULL on failure
  */
-avl_t *convert_to_bst(int *array, int beginning_index, int end_index)
+avl_t *convert_to_avl(int *array, int begin_idx, int end_idx, avl_t *parent)
 {
 	avl_t *new_node = NULL;
 	int middle_index = 0;
 
-	if (end_index < beginning_index)
+	if (end_idx < begin_idx)
 		return (NULL);
 
 	new_node = malloc(sizeof(avl_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	middle_index = (beginning_index + end_index) / 2;
+	middle_index = (begin_idx + end_idx) / 2;
 
 	new_node->n = array[middle_index];
-	new_node->left = convert_to_bst(array, beginning_index, middle_index - 1);
-	new_node->right = convert_to_bst(array, middle_index + 1, end_index);
+	new_node->parent = parent;
+	new_node->left = convert_to_avl(array, begin_idx, middle_index - 1, new_node);
+	new_node->right = convert_to_avl(array, middle_index + 1, end_idx, new_node);
+
 	return (new_node);
 }
 
@@ -46,5 +49,5 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 	if (array == NULL || size == 0)
 		return (NULL);
 
-	return (convert_to_bst(array, 0, size - 1));
+	return (convert_to_avl(array, 0, size - 1, NULL));
 }
