ec122f5938515eb92391505006b3a385fc360e8b
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-16 16:19:21 +0200

    change linear_skip function prototype

--- skip_list/search.h
+++ skip_list/search.h
@@ -30,6 +30,6 @@ typedef struct skiplist_s
 skiplist_t *create_skiplist(int *array, size_t size);
 void print_skiplist(const skiplist_t *list);
 void free_skiplist(skiplist_t *list);
-skiplist_t *linear_skip(skiplist_t *head, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif /* _SEARCH_H_ */
