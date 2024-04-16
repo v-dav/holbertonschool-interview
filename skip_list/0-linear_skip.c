eef8e8a4a50ff1bc76f7a5d61990ceca36f534c9
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-16 17:48:52 +0200

    write linear Linear search in skip list algo

---
+++ skip_list/0-linear_skip.c
@@ -0,0 +1,53 @@
+#include "search.h"
+
+/**
+ * linear_skip - searches for a value in a sorted skip list of integers
+ *
+ * @head: Pointer to the head node of the list
+ * @value: the value to search for
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *search_pointer;
+
+	if (!list)
+		return (NULL);
+
+	search_pointer = list->express;
+
+	printf("Value checked at index [%lu] = [%d]\n",
+	       search_pointer->index, search_pointer->n);
+
+	while (search_pointer && search_pointer->n < value)
+	{
+		list = search_pointer;
+		search_pointer = search_pointer->express;
+
+		if (!search_pointer)
+			break;
+
+		printf("Value checked at index [%lu] = [%d]\n",
+		       search_pointer->index, search_pointer->n);
+	}
+
+	if (!search_pointer)
+	{
+		search_pointer = list;
+		while (search_pointer->next)
+			search_pointer = search_pointer->next;
+	}
+
+	printf("Value found between indexes [%lu] and [%lu]\n",
+	       list->index, search_pointer->index);
+
+	while (list && list->n <= search_pointer->n)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", list->index,
+		       list->n);
+		if (list->n == value)
+			return (list);
+		list = list->next;
+	}
+
+	return (NULL);
+}
