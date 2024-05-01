4aa219766a90ebdc7ecec4b6a5be461596ab03b0
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-05-01 14:07:28 +0200

    fix pep8 style

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -4,9 +4,9 @@
 
 def rain(walls):
     """The core algorithms of the rain computation
-    
+
     DOESN'T WORKING FOR "TETRIS LIKE" TOPOGRAPHY
-    
+
     """
 
     total_area = 0
@@ -16,17 +16,17 @@ def rain(walls):
 
     for index in range(len(walls)):
         current_height = walls[index]
-        
+
         # Find the hole
         if current_height == 0:
             # Define the left wall
             if (index - 1) >= 0 and walls[index - 1] != 0:
                 left_wall = walls[index - 1]
-            
+
             # Define the right wall
             if (index + 1) < len(walls):
                 right_wall = walls[index + 1]
-            
+
             # Update the bottom length
             if left_wall != 0:
                 height_left = left_wall
@@ -34,7 +34,7 @@ def rain(walls):
 
                 if right_wall != 0:
                     height_right = right_wall
-                    
+
                     # Calculate the area of a hole containing water
                     if height_left and height_right:
                         min_height = min(height_right, height_left)
