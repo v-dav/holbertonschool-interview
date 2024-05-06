2c4155611ae27339ae7a245b7b7b219c6a29a31c
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-05-06 10:44:37 +0200

    set up project basics

---
+++ heap_sort/0-heap_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ * heap_sort - function that sorts an array of integers
+ * in ascending order using the Heap sort algorithm
+ * 
+ * @array: the array to sort
+ * @size: number of elements in @array
+ * 
+ * Return: void
+ */
+void heap_sort(int *array, size_t size)
+{
+
+}
