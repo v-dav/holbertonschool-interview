aaf97e49f69731da33089e8b42c539c2647f3914
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-09-11 16:23:36 +0200

    refactor the documentation

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -1,24 +1,9 @@
 #!/usr/bin/python3
-""" 0-prime_game.py """
+"""Prime game module"""
 
 
 def isWinner(x, nums):
-    """
-    Determines the winner of a set of prime number removal games.
-
-    Args:
-        x (int): The number of rounds.
-        nums (list of int): A list of integers where each integer n denotes
-        a set of consecutive integers starting from 1 up to and including n.
-
-    Returns:
-        str: The name of the player who won the most rounds (either "Ben"
-        or "Maria").
-        None: If the winner cannot be determined.
-
-    Raises:
-        None.
-    """
+    """Determines the winner of the prime game"""
     if x <= 0 or nums is None:
         return None
     if x != len(nums):
@@ -46,20 +31,7 @@ def isWinner(x, nums):
 
 
 def rm_multiples(ls, x):
-    """
-    Removes multiples of a prime number from an array of possible prime
-    numbers.
-
-    Args:
-        ls (list of int): An array of possible prime numbers.
-        x (int): The prime number to remove multiples of.
-
-    Returns:
-        None.
-
-    Raises:
-        None.
-    """
+    """Removes multiples of a prime number"""
     for i in range(2, len(ls)):
         try:
             ls[i * x] = 0
